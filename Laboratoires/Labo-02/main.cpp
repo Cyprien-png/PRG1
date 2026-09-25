ff3c5dff50c41d4beea522db412efa1e6e9297b5
Cyprien-png <cyprien04@hotmail.com>
2026-09-25 16:31:40 +0200

    feat: add the init prompt

--- Laboratoires/Labo-02/main.cpp
+++ Laboratoires/Labo-02/main.cpp
@@ -1,7 +1,21 @@
-/* --------------------------- 
-Laboratoire : 02
-Auteur(s) : 
-Date : 
-But : Calcul du temps de trajet 
-Remarque(s) : 
---------------------------- */
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Calculate the WALL-E's moving time
+ */
+int main () {
+    int dx = 3;
+    int dy = 10;
+    int s1 = 5;
+    int s2 = 2;
+    int L1 = 6;
+
+    cout << "Hello there!" << endl;
+    cout << "This program is about to compute how long WALL-E's moving to the object." << endl;
+    cout << "Here are the data:" << endl;
+    cout << "On a " << dy << "km road, he will move at " << s1 << "km/h" << endl;
+    cout << "On a dirt path, he will move at " << s2 << "km/h" << endl;
+    cout << "The length of the dirt path will be calculated" << endl;
+}
