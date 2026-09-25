3279cffbb3e63b783a831d5a2b513fe81a093a84
Cyprien-png <cyprien04@hotmail.com>
2026-09-25 16:47:27 +0200

    feat: add the computations + goodbye prompt

--- Laboratoires/Labo-02/main.cpp
+++ Laboratoires/Labo-02/main.cpp
@@ -6,16 +6,28 @@ using namespace std;
  * Calculate the WALL-E's moving time
  */
 int main () {
-    int dx = 3;
-    int dy = 10;
-    int s1 = 5;
-    int s2 = 2;
-    int L1 = 6;
+    float dx = 3.0f;
+    float dy = 10.0f;
+    float s1 = 5.0f;
+    float s2 = 2.0f;
+    float L1 = 6.0f;
+    string separator = "========================================";
 
+    cout << separator << endl;
     cout << "Hello there!" << endl;
     cout << "This program is about to compute how long WALL-E's moving to the object." << endl;
     cout << "Here are the data:" << endl;
     cout << "On a " << dy << "km road, he will move at " << s1 << "km/h" << endl;
     cout << "On a dirt path, he will move at " << s2 << "km/h" << endl;
-    cout << "The length of the dirt path will be calculated" << endl;
+    cout << "The length of the dirt path has to be calculated" << endl;
+    cout << separator << endl;
+
+    // calculate the hypotenuse of the dirt path (L2)
+    float L2 = (sqrt(pow((dy - L1), 2) + pow(dx, 2)));
+
+    float timeToTravel1 = L1 / s1;
+    float timeToTravel2 = L2 / s2;
+    float totalTime = timeToTravel1 + timeToTravel2;
+
+    cout << "WALL-E's about to take " << totalTime << " hours to reach the object on a " << L1 + L2 << "km long way"<< endl;
 }
